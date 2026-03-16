fbfc11a3d3d09ceba1b52f59ef236e7a308b8035
unknown <yayanrifan45@gmail.com>
2026-03-16 10:56:15 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-red-500 ">Tugas-1 Pemrograman Sisi Klien</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem
+        omnis ipsum provident pariatur suscipit reprehenderit eius, recusandae
+        voluptatum quod! Delectus excepturi sunt quasi eum in vitae perspiciatis
+        magni ipsum dicta ullam itaque temporibus, omnis obcaecati rerum, earum
+        minima, possimus illum saepe est ipsa optio? Consectetur labore quis
+        amet architecto natus quasi unde aspernatur neque exercitationem,
+        corrupti ipsum aut tempora laudantium atque sit aliquid sapiente ab
+        beatae maxime minus culpa in. Ipsum officia culpa impedit, temporibus
+        iure, quidem deserunt illum fugiat vitae laudantium veniam ad inventore
+        mollitia nostrum similique unde nisi cupiditate amet pariatur
+        reprehenderit optio harum. Tenetur, id. Nulla, iste? Accusantium earum
+        odio, maxime quisquam itaque similique numquam velit assumenda mollitia
+        sit? Sit sapiente facilis similique culpa itaque, perferendis neque
+        placeat architecto est praesentium. Repudiandae in eius nostrum
+        obcaecati impedit non nobis nesciunt officia. Accusantium, commodi est
+        dolorem vel eveniet velit sit rerum. Eveniet fugit cum minus sapiente
+        illo. Quod tempora nesciunt a esse nostrum voluptates repellat id. Quod
+        eum, quo delectus doloremque dolorem autem harum eligendi nesciunt
+        aliquid neque nemo assumenda non recusandae laboriosam ut voluptatum,
+        deserunt tempore blanditiis adipisci mollitia a reiciendis molestias?
+        Iusto vero non nesciunt laborum id, quam totam nostrum, debitis vitae
+        odit quas veniam cupiditate illum officiis tempora, aliquid eaque
+        deleniti alias omnis doloremque fuga expedita. Laborum, distinctio
+        dolores debitis, quibusdam est maiores dolorum voluptatibus eius nemo
+        reprehenderit commodi quia quam. Quos, adipisci nihil, quia optio, autem
+        nostrum doloribus blanditiis necessitatibus voluptatibus soluta
+        exercitationem ipsa sapiente similique illum. Quam itaque qui dolores
+        labore veritatis reiciendis recusandae impedit quis omnis. Tempora
+        maxime dolor neque sapiente. Laborum beatae velit architecto corrupti
+        veniam tempora nostrum exercitationem adipisci neque aliquam, similique,
+        sequi saepe obcaecati totam consequatur consequuntur, perferendis quod
+        officiis atque aut debitis placeat. Corporis quasi aliquid vel suscipit
+        tenetur sed alias. Dolorum quis doloremque tenetur recusandae voluptatum
+        voluptate asperiores fugiat cum quo aliquam! Debitis qui facere id
+        explicabo suscipit aspernatur vero dolorum! Nisi illum ab possimus alias
+        veniam amet accusantium vel nobis in quos. Nostrum consequuntur atque
+        debitis nesciunt repellat! Similique laborum totam dignissimos porro.
+        Ratione, facilis at eum culpa optio magnam fugiat commodi ad! Laboriosam
+        necessitatibus in voluptates explicabo, similique accusamus, nam
+        possimus quaerat consequatur expedita dolores dolor illum aspernatur quo
+        molestiae? Exercitationem aspernatur consectetur soluta, eum dolorem
+        impedit harum deleniti a! Atque eligendi debitis, quod iste veniam
+        ratione mollitia vero impedit, perspiciatis eum facere at ipsam sint
+        culpa aliquid corporis accusamus praesentium doloribus asperiores! Rem,
+        reiciendis?
+      </p>
+    </>
+  );
+}
+
+export default App;

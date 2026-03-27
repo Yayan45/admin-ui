e84b30a1f1a22f58bcddf37543b24d4ecf810fa1
unknown <yayanrifan45@gmail.com>
2026-03-27 19:46:18 +0700

    menggunakan utility class tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,47 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <>
-      <h1 className="bg-red-500 ">Tugas-1 Pemrograman Sisi Klien</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem
-        omnis ipsum provident pariatur suscipit reprehenderit eius, recusandae
-        voluptatum quod! Delectus excepturi sunt quasi eum in vitae perspiciatis
-        magni ipsum dicta ullam itaque temporibus, omnis obcaecati rerum, earum
-        minima, possimus illum saepe est ipsa optio? Consectetur labore quis
-        amet architecto natus quasi unde aspernatur neque exercitationem,
-        corrupti ipsum aut tempora laudantium atque sit aliquid sapiente ab
-        beatae maxime minus culpa in. Ipsum officia culpa impedit, temporibus
-        iure, quidem deserunt illum fugiat vitae laudantium veniam ad inventore
-        mollitia nostrum similique unde nisi cupiditate amet pariatur
-        reprehenderit optio harum. Tenetur, id. Nulla, iste? Accusantium earum
-        odio, maxime quisquam itaque similique numquam velit assumenda mollitia
-        sit? Sit sapiente facilis similique culpa itaque, perferendis neque
-        placeat architecto est praesentium. Repudiandae in eius nostrum
-        obcaecati impedit non nobis nesciunt officia. Accusantium, commodi est
-        dolorem vel eveniet velit sit rerum. Eveniet fugit cum minus sapiente
-        illo. Quod tempora nesciunt a esse nostrum voluptates repellat id. Quod
-        eum, quo delectus doloremque dolorem autem harum eligendi nesciunt
-        aliquid neque nemo assumenda non recusandae laboriosam ut voluptatum,
-        deserunt tempore blanditiis adipisci mollitia a reiciendis molestias?
-        Iusto vero non nesciunt laborum id, quam totam nostrum, debitis vitae
-        odit quas veniam cupiditate illum officiis tempora, aliquid eaque
-        deleniti alias omnis doloremque fuga expedita. Laborum, distinctio
-        dolores debitis, quibusdam est maiores dolorum voluptatibus eius nemo
-        reprehenderit commodi quia quam. Quos, adipisci nihil, quia optio, autem
-        nostrum doloribus blanditiis necessitatibus voluptatibus soluta
-        exercitationem ipsa sapiente similique illum. Quam itaque qui dolores
-        labore veritatis reiciendis recusandae impedit quis omnis. Tempora
-        maxime dolor neque sapiente. Laborum beatae velit architecto corrupti
-        veniam tempora nostrum exercitationem adipisci neque aliquam, similique,
-        sequi saepe obcaecati totam consequatur consequuntur, perferendis quod
-        officiis atque aut debitis placeat. Corporis quasi aliquid vel suscipit
-        tenetur sed alias. Dolorum quis doloremque tenetur recusandae voluptatum
-        voluptate asperiores fugiat cum quo aliquam! Debitis qui facere id
-        explicabo suscipit aspernatur vero dolorum! Nisi illum ab possimus alias
-        veniam amet accusantium vel nobis in quos. Nostrum consequuntur atque
-        debitis nesciunt repellat! Similique laborum totam dignissimos porro.
-        Ratione, facilis at eum culpa optio magnam fugiat commodi ad! Laboriosam
-        necessitatibus in voluptates explicabo, similique accusamus, nam
-        possimus quaerat consequatur expedita dolores dolor illum aspernatur quo
-        molestiae? Exercitationem aspernatur consectetur soluta, eum dolorem
-        impedit harum deleniti a! Atque eligendi debitis, quod iste veniam
-        ratione mollitia vero impedit, perspiciatis eum facere at ipsam sint
-        culpa aliquid corporis accusamus praesentium doloribus asperiores! Rem,
-        reiciendis?
-      </p>
+      <div className="min-h-screen bg-neutral-800 flex flex-col items-center justify-center text-center">
+        <div className="flex items-center justify-center space-x-8 mb-12">
+          <a href="https://vite.dev" target="_blank">
+            <img src={viteLogo} className="w-24 h-24" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img
+              src={reactLogo}
+              className="w-24 h-24 animate-spin"
+              alt="React logo"
+              style={{ animationDuration: "10s" }}
+            />
+          </a>
+        </div>
+
+        <h1 className="text-white text-6xl font-bold mb-16">Vite + React</h1>
+
+        <div className="text-sm space-y-6 mb-8">
+          <button
+            onClick={() => setCount((count) => count + 1)}
+            className="bg-neutral-900 text-white py-2 px-6 rounded-lg text-lg"
+          >
+            count is {count}
+          </button>
+
+          <p className="text-neutral-400">
+            Edit <code>src/App.jsx</code> and save to test HMR
+          </p>
+        </div>
+
+        <p className="text-neutral-400">
+          Click on the Vite and React logos to learn more
+        </p>
+      </div>
     </>
   );
 }

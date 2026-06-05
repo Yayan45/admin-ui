c0e8137f8b8d6b6c37af8a173f360032cab2f8e0
unknown <yayanrifan45@gmail.com>
2026-06-05 14:05:32 +0700

    14-State-Management-with-Contex

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 // import Parent from "./latihan/Parent.jsx";
-import { ThemeContextProvider } from "./context/themeContext.jsx";
+import { ThemeContextProvider } from "./context/ThemeContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
